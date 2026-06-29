add5d726f147f321752e67c7b5f826d90a996418
BL-Anbu <anbarasu.annadurai@bridgelabz.com>
2026-06-29 15:44:23 +0530

    [Anbu] Added : UC15 Log4j Migration for Professional Logging

---
+++ ClinicOpsApp/src/com/bridgelabz/clinicops/Test.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.clinicops;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Test {
+
+    private static final Logger logger =
+            LogManager.getLogger(Test.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("Debug Message");
+
+        logger.info("Information");
+
+        logger.warn("Warning");
+
+        logger.error("Error");
+    }
+}
